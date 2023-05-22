02c7123a6701696ca56f8bcd05587651edb0005c
Lokesh Chandra Das <ldas@mathsci-h2wg319lq6nw.uom.memphis.edu>
2023-05-22 15:35:57 -0500

    SUMO missing file updated

--- train_sumo_gym.py
+++ train_sumo_gym.py
@@ -4,5 +4,5 @@ from dqn_sumo_gym import Agent
 
 if __name__ == '__main__':
     agent = Agent("Agent")
-    env = gym.make("sumo-v0", render_mode="")
+    env = gym.make("sumo-v0", render_mode="human")
     agent.train_RL(env)
